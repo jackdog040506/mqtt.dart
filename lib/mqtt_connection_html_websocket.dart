fbc2a61c7f8d3821fe2813da710784208d58e68c
James <james@infinitiessoft.com>
2018-03-16 14:49:51 +0800

    update websocket with RCS-6455

--- lib/mqtt_connection_html_websocket.dart
+++ lib/mqtt_connection_html_websocket.dart
@@ -5,29 +5,29 @@ import 'dart:async';
 import 'mqtt_shared.dart';
 import 'dart:typed_data';
 
-class MqttConnectionHtmlWebSocket extends VirtualMqttConnection{
+class MqttConnectionHtmlWebSocket extends VirtualMqttConnection {
   final String _url;
   WebSocket _ws;
-  
+
   MqttConnectionHtmlWebSocket.setOptions(this._url);
-  
+
   Future connect() {
     print("[HTML WebSocket] Connecting to $_url");
-    
+
     Completer connected = new Completer();
-    _ws = new WebSocket(_url);
+    _ws = new WebSocket(_url, "mqtt");
     _ws.binaryType = "arraybuffer";
     _ws.onOpen.listen((e) {
       connected.complete(_ws);
     });
-    
-    return connected.future;    
+
+    return connected.future;
   }
-  
+
   handleConnectError(e) {
     print("Error: $e");
   }
-  
+
   privateSendMessageToBroker(MqttMessage m) {
     print("[HTML WebSocket] SendMessage ${m.buf}");
     _ws.send(new Uint8List.fromList(m.buf));
@@ -36,13 +36,13 @@ class MqttConnectionHtmlWebSocket extends VirtualMqttConnection{
   setConnection(cnx) {
     //_ws = cnx;
   }
-  
+
   startListening(_processData, _handleDone, _handleError) {
-    _ws.onClose.listen( (e) => _handleDone());
-    _ws.onMessage.listen( (MessageEvent e) => _processData(e.data) );
-    _ws.onError.listen( (e) => _handleError(e) );    
+    _ws.onClose.listen((e) => _handleDone());
+    _ws.onMessage.listen((MessageEvent e) => _processData(e.data));
+    _ws.onError.listen((e) => _handleError(e));
   }
-  
+
   close() {
     _ws.close();
   }
